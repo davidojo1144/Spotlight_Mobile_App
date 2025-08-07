9b67a6f64202c5242c194d3153f58888c69dffb6
davidojo1144 <davco1144@gmail.com>
2025-08-07 02:48:54 +0100

    Fullstack Spotlight Mobile App

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,10 +1,21 @@
 import { Stack } from "expo-router";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 
+import { ClerkProvider } from '@clerk/clerk-expo'
+import { Slot } from 'expo-router'
+
+function RootLayoutNav() {
+  return (
+    <ClerkProvider>
+      <Slot />
+    </ClerkProvider>
+  )
+}
+
 export default function RootLayout() {
   return ( 
     <SafeAreaProvider>
-      <SafeAreaView style={{flex: 1}}>
+      <SafeAreaView style={{flex: 1, backgroundColor: "black"}}>
         <Stack screenOptions={{headerShown: false}}/>
       </SafeAreaView>
     </SafeAreaProvider>
