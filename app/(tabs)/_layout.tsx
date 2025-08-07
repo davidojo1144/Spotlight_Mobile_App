76d6862ffb93bd5be8c3d7ba71343ada485f8e66
davidojo1144 <davco1144@gmail.com>
2025-08-07 01:50:51 +0100

    Fullstack Spotlight Mobile App

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -10,7 +10,15 @@ export default function Tablayout() {
       tabBarShowLabel: false,
       headerShown: false,
       tabBarActiveTintColor: COLORS.primary,
-      tabBarInactiveTintColor: COLORS.grey
+      tabBarInactiveTintColor: COLORS.grey,
+      tabBarStyle: {
+          backgroundColor: "black",
+          borderTopWidth: 0,
+          position: "absolute",
+          elevation: 0,
+          height: 60,
+          paddingBottom: 8,
+        },
     }}>
       <Tabs.Screen 
       name="index"
